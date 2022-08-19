0c3820cf9386a3f7c038995f69f54effc86c9744
Stefan Weiz <s.weiz@forsit.de>
2022-08-19 15:30:47 +0200

    Implements automatic versioning

--- build-and-push-docker.sh
+++ build-and-push-docker.sh
@@ -1,8 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-version=13
+if [[ ! -f version ]]; then
+    echo -n 0 > version
+fi
+
+version=$(cat version)
+version=$((version+1))
 
 docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} . && \
 docker tag bausparkadse/dind-kube-skaffold-aws:${version} bausparkadse/dind-kube-skaffold-aws:latest && \
 docker push bausparkadse/dind-kube-skaffold-aws:${version} && \
 docker push bausparkadse/dind-kube-skaffold-aws:latest
+
+echo -n ${version} > version
+
+git add version
+git commit -m "Makes new Docker image available"
+git push
