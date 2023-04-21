2a09adc0cd84dc989476853647422060043d97c2
Stefan Weiz <s.weiz@forsit.de>
2023-04-21 15:53:10 +0200

    Pushes images at the end of all builds

--- build-and-push-docker.sh
+++ build-and-push-docker.sh
@@ -8,19 +8,19 @@ version=$(cat version)
 version=$((version+1))
 
 docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} -f Dockerfile.1.24 . && \
-docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.24 && \
-docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.24
+docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.24
 
 docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} -f Dockerfile.1.25 . && \
-docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.25 && \
-docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.25
+docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.25
 
 docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} -f Dockerfile.1.26 . && \
-docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.26 && \
-docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.26
+docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.26
 
 docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} -f Dockerfile.1.27 . && \
 docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.27 bausparkadse/dind-kube-skaffold-aws:latest && \
+docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.24 && \
+docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.25 && \
+docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.26 && \
 docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.27 && \
 docker push bausparkadse/dind-kube-skaffold-aws:latest
 
