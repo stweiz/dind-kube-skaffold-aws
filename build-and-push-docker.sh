605291e3fc6945c75b029428e4e99f470a750e7a
Stefan Weiz <s.weiz@forsit.de>
2023-04-21 15:48:41 +0200

    Builds different kubectl versions for supported Kuberentes cluster versions

--- build-and-push-docker.sh
+++ build-and-push-docker.sh
@@ -7,9 +7,21 @@ fi
 version=$(cat version)
 version=$((version+1))
 
-docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} . && \
-docker tag bausparkadse/dind-kube-skaffold-aws:${version} bausparkadse/dind-kube-skaffold-aws:latest && \
-docker push bausparkadse/dind-kube-skaffold-aws:${version} && \
+docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} -f Dockerfile.1.24 . && \
+docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.24 && \
+docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.24
+
+docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} -f Dockerfile.1.25 . && \
+docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.25 && \
+docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.25
+
+docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} -f Dockerfile.1.26 . && \
+docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.26 && \
+docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.26
+
+docker build --pull -t bausparkadse/dind-kube-skaffold-aws:${version} -f Dockerfile.1.27 . && \
+docker tag bausparkadse/dind-kube-skaffold-aws:${version}-1.27 bausparkadse/dind-kube-skaffold-aws:latest && \
+docker push bausparkadse/dind-kube-skaffold-aws:${version}-1.27 && \
 docker push bausparkadse/dind-kube-skaffold-aws:latest
 
 echo -n ${version} > version
